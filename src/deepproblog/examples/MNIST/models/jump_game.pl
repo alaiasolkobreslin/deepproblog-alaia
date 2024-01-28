2cbc97c6332ead8770c928c906055b81546caecf
Alaia Solko-Breslin <ajs644@cornell.edu>
2024-01-28 21:39:08 +0000

    jump game

--- src/deepproblog/examples/MNIST/models/jump_game.pl
+++ src/deepproblog/examples/MNIST/models/jump_game.pl
@@ -1,11 +1,7 @@
 nn(mnist_net,[X],Y,[0,1,2,3,4,5,6,7,8,9]) :: digit(X,Y).
 
-number([],Result,Result).
-number([H|T],Acc,Result) :- digit(H,Nr),Acc2 is Nr+10*Acc,number(T,Acc2,Result).
-number(X,Y) :- number(X,0,Y).
-
 num_list([], []).
-num_list([H|T], [N|TN]) :- number(H, N), num_list(T, TN).
+num_list([H|T], [N|TN]) :- digit(H, N), num_list(T, TN).
 
 reachable([_]).
 reachable([H|T]):- length(T, N), H >= N.
@@ -17,5 +13,9 @@ reachable([H|T]):- append([H1|T1], [H2|T2], T),
 % Assume list of lists as input [[3], [0], [1]]
 % return 1 for true, 0 for false
 jump_game(X, Z) :- num_list(X, L),
-                   reachable(L), !, Z is 1.
-jump_game(_, Z) :- Z is 0.
+                   reachable(L), Z is 1.
+% jump_game(_, Z) :- Z is 0.
+jump_game(X, Z) :- num_list(X, L),
+                   not(reachable(L)), Z is 0.
+
+main(A, B, C, D, E, Z) :- jump_game([A, B, C, D, E], Z).
